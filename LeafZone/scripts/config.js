65df77dfffec9da1806d064c13b0ec70bccba49f
Svetozar Toskov <s.toskov@abv.bg>
2014-03-27 18:00:25 -0500

    Changes made by Svetozar Toskov (s.toskov@abv.bg).

---
+++ LeafZone/scripts/config.js
@@ -0,0 +1,22 @@
+(function (global) {
+    var app = global.app = global.app || {};
+    
+    app.config = {        
+        everlive: {
+            apiKey: "woLBIbIHeu9aWBrb",
+            scheme: "http"
+        },        
+        facebook: {
+            appId: "",
+            redirectUri: ""
+        },        
+        google: {
+            clientId:"",
+            redirectUri: ""
+        },        
+        liveId: {
+            clientId:"",
+            redirectUri: ""       
+        }      
+    };    
+})(window);
