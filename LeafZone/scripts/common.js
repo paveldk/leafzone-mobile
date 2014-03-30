c0c062264c17bc060116f210ac20285740eb25d5
Svetozar Toskov <s.toskov@abv.bg>
2014-03-29 21:01:43 -0500

    Bind sign in and sign up views

--- LeafZone/scripts/common.js
+++ LeafZone/scripts/common.js
@@ -1,11 +1,26 @@
 (function (global) {
-    var app = global.app = global.app || {};
-    
-    function notification(title, message){
-        navigator.notification.alert(title, function () { }, message, "OK");
-    }
-    
-    app.common = {        
-        notification: notification
-    };    
+	var app = global.app = global.app || {};
+
+	function notification(title, message) {
+		navigator.notification.alert(message, function () { }, title, "OK");
+	}
+
+	function showLoading() {
+		kendo.mobile.application.showLoading();
+	}
+
+	function hideLoading() {
+		kendo.mobile.application.hideLoading();
+	}
+
+	function navigateToView(view) {
+		kendo.mobile.application.navigate(view);
+	}
+
+	app.common = {
+		notification: notification,
+		showLoading: showLoading,
+		hideLoading: hideLoading,
+		navigateToView: navigateToView
+	};
 })(window);
--- LeafZone/scripts/config.js
+++ LeafZone/scripts/config.js
@@ -1,33 +1,34 @@
 (function (global) {
-    var app = global.app = global.app || {};
-    
-    app.config = {        
-        everlive: {
-            //The original api key is woLBIbIHeu9aWBrb, but changed temporarily, becase of acount limitations
-            apiKey: "ltG65jxbc26oEeod",
-            scheme: "http"
-        },        
-        //TODO: create a new id
-        facebook: {
-            appId: "1408629486049918",
-            redirectUri: "https://www.facebook.com/connect/login_success.html",
-            endpoint: "https://www.facebook.com/dialog/oauth"
-        },        
-        //TODO: create a new id
-        google: {
-            clientId:"406987471724-q1sorfhhcbulk6r5r317l482u9f62ti8.apps.googleusercontent.com",
-            redirectUri: "http://localhost",
-            scope: "https://www.googleapis.com/auth/userinfo.profile",
-            endpoint: "https://accounts.google.com/o/oauth2/auth"
-        },        
-        //TODO: create a new id
-        liveId: {
-            clientId: "000000004C10D1AF",
-            redirectUri: "https://login.live.com/oauth20_desktop.srf"       
-        },
-        views: {
-            home: "#",
-            login: ""
-        }
-    };    
+	var app = global.app = global.app || {};
+
+	app.config = {
+		everlive: {
+			//The original api key is woLBIbIHeu9aWBrb, but changed temporarily, becase of acount limitations
+			apiKey: "ltG65jxbc26oEeod",
+			scheme: "http"
+		},
+		//TODO: create a new id
+		facebook: {
+			appId: "1408629486049918",
+			redirectUri: "https://www.facebook.com/connect/login_success.html",
+			endpoint: "https://www.facebook.com/dialog/oauth"
+		},
+		//TODO: create a new id
+		google: {
+			clientId: "406987471724-q1sorfhhcbulk6r5r317l482u9f62ti8.apps.googleusercontent.com",
+			redirectUri: "http://localhost",
+			scope: "https://www.googleapis.com/auth/userinfo.profile",
+			endpoint: "https://accounts.google.com/o/oauth2/auth"
+		},
+		//TODO: create a new id
+		liveId: {
+			clientId: "000000004C10D1AF",
+			redirectUri: "https://login.live.com/oauth20_desktop.srf"
+		},
+		views: {
+			home: "#home-view",
+			signIn: "scripts/modules/login/signin.html",
+			signUp: "scripts/modules/login/signup.html"
+		}
+	};
 })(window);
