b47921f6c24f42a26806075db666479d0b4c13ee
Svetozar Toskov <s.toskov@abv.bg>
2014-04-09 15:35:22 -0500

    init disease details view

--- LeafZone/scripts/modules/diseases/disease-details.js
+++ LeafZone/scripts/modules/diseases/disease-details.js
@@ -1,16 +1,13 @@
 (function (global) {
-	var PlantDetailsViewModel,
-		PlantDetailsService,
+	var DiseaseDetailsViewModel,
+		DiseaseDetailsService,
         app = global.app = global.app || {};
 
-	PlantDetailsViewModel = kendo.data.ObservableObject.extend({
+	DiseaseDetailsViewModel = kendo.data.ObservableObject.extend({
 		imageId: "",
 		name: "",
-		botanicalName: "",
-		ornamentalCharacteristics: "",
-		environmentalCharacteristics: "",
-		insectDiseaseCharacteristics: "",
-		cultivars: "",
+		control: "",
+		symptoms: "",	
 		images: null,
 
 		init: function () {
@@ -31,13 +28,13 @@
         }
 	});
 
-	PlantDetailsService = kendo.Class.extend({
+	DiseaseDetailsService = kendo.Class.extend({
 		viewModel: null,
 
 		init: function () {
 			var that = this;
 
-			that.viewModel = new PlantDetailsViewModel();
+			that.viewModel = new DiseaseDetailsViewModel();
 			that.showData = $.proxy(that.initData, that);
 		},
 
@@ -51,7 +48,7 @@
 
 			app.common.showLoading();
 
-			app.everlive.data("Plants").getById(dataId)
+			app.everlive.data("Disease").getById(dataId)
 			.then($.proxy(that.setData, that))
 			.then(null, $.proxy(that.onError, that));			
 		},
@@ -62,11 +59,8 @@
 			that.viewModel.set("imageId", plantData.result.MainImage);
 			that.viewModel.set("images", plantData.result.Images);
 			that.viewModel.set("name", plantData.result.Name);			
-			that.viewModel.set("botanicalName", plantData.result.BotanicalName);	
-			that.viewModel.set("ornamentalCharacteristics", that.parseMultiLinesText(plantData.result.OrnamentalCharacteristics || ""));	
-			that.viewModel.set("environmentalCharacteristics", that.parseMultiLinesText(plantData.result.EnvironmentalCharacteristics || ""));	
-			that.viewModel.set("insectDiseaseCharacteristics", that.parseMultiLinesText(plantData.result.InsectDiseaseCharacteristics || ""));	
-			that.viewModel.set("cultivars", that.parseMultiLinesText(plantData.result.Cultivars || ""));	
+			that.viewModel.set("control", that.parseMultiLinesText(plantData.result.Control || ""));	
+			that.viewModel.set("symptoms", that.parseMultiLinesText(plantData.result.Symptoms || ""));	
 			
 			app.common.hideLoading();
 		},
@@ -81,5 +75,5 @@
 		}
 	});
 
-	app.plantDetailsService = new PlantDetailsService();
+	app.diseaseDetailsService = new DiseaseDetailsService();
 })(window);
