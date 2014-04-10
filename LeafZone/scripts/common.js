9c3e096d90ac20e7e7e1e18a26637d1557766759
Svetozar Toskov <s.toskov@abv.bg>
2014-04-10 16:29:39 -0500

    Load tumbnails

--- LeafZone/scripts/common.js
+++ LeafZone/scripts/common.js
@@ -1,31 +1,33 @@
 (function (global) {
-    var app = global.app = global.app || {};
+	var app = global.app = global.app || {};
 
-    function notification(title, message) {
-        navigator.notification.alert(message, function () {}, title, "OK");
-    }
+	app.images = app.imagesList || {};
 
-    function showLoading() {
-        kendo.mobile.application.showLoading();
-    }
+	function notification(title, message) {
+		navigator.notification.alert(message, function () { }, title, "OK");
+	}
 
-    function hideLoading() {
-        kendo.mobile.application.hideLoading();
-    }
+	function showLoading() {
+		kendo.mobile.application.showLoading();
+	}
 
-    function navigateToView(view) {
-        kendo.mobile.application.navigate(view);
-    }
+	function hideLoading() {
+		kendo.mobile.application.hideLoading();
+	}
+
+	function navigateToView(view) {
+		kendo.mobile.application.navigate(view);
+	}
 
 
-    function resizeImage(url, targetWidth, targetHeight, crop) {
-        var imageObj = new Image();
+	function resizeImage(url, targetWidth, targetHeight, crop) {
+		var imageObj = new Image();
 
-        imageObj.crossOrigin = "Anonymous";
+		imageObj.crossOrigin = "Anonymous";
 
-        return new RSVP.Promise(function (resolve, reject) {
-            imageObj.onload = function () {
-                var context,
+		return new RSVP.Promise(function (resolve, reject) {
+			imageObj.onload = function () {
+				var context,
                     ratio,
                     imageData,
                     result,
@@ -35,45 +37,89 @@
                     heightOffset = 0,
                     canvas = document.createElement("canvas");
 
-                if (width > height) {
-                    ratio = targetHeight / height;
-                } else {
-                    ratio = targetWidth / width;
-                }
+				if (width < height) {
+					ratio = targetHeight / height;
+				} else {
+					ratio = targetWidth / width;
+				}
 
-                canvas.width = width * ratio;
-                canvas.height = height * ratio;
+				canvas.width = width * ratio;
+				canvas.height = height * ratio;
 
-                widthOffset = (canvas.width - targetWidth) / 2;
-                heightOffset = (canvas.height - targetHeight) / 2;
+				widthOffset = (canvas.width - targetWidth) / 2;
+				heightOffset = (canvas.height - targetHeight) / 2;
 
-                context = canvas.getContext("2d");
-                context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+				context = canvas.getContext("2d");
+				context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
 
 
-                if (crop) {
-                    imageData = context.getImageData(widthOffset, heightOffset, targetWidth, targetHeight);
+				if (crop) {
+					imageData = context.getImageData(widthOffset, heightOffset, targetWidth, targetHeight);
 
-                    canvas.width = targetWidth;
-                    canvas.height = targetHeight;
-                    context = canvas.getContext("2d");
-                    context.putImageData(imageData, 0, 0);
-                }
+					canvas.width = targetWidth;
+					canvas.height = targetHeight;
+					context = canvas.getContext("2d");
+					context.putImageData(imageData, 0, 0);
+				}
 
-                result = canvas.toDataURL("image/jpeg");
+				result = canvas.toDataURL("image/jpeg");
 
-                resolve(result);
-            };
+				resolve(result);
+			};
 
-            imageObj.src = url;
-        })
-    }
+			imageObj.src = url;
+		})
+	}
 
-    app.common = {
-        notification: notification,
-        showLoading: showLoading,
-        hideLoading: hideLoading,
-        navigateToView: navigateToView,
-		resizeImage: resizeImage
-    };
+	function updateFilesInfo() {
+		var q = new Everlive.Query();
+
+		q.select("Filename", "Uri", "Id");
+
+		return app.everlive.data("Files").get(q)
+		.then(function (data) {
+			var currentItem;
+
+			app.images.idMap = {};
+			app.images.nameMap = {};
+
+
+			for (var i = 0; i < data.result.length; i++) {
+				currentItem = data.result[i];
+
+				app.images.idMap[currentItem.Id] = currentItem;
+				app.images.nameMap[currentItem.Filename] = currentItem;
+			}
+
+			return data.result;
+		});
+	}	
+
+	function getTumbnailIdByImageId(imageId) {
+		var fileName,
+			tumbnailName;
+
+		if (!app.images.idMap[imageId]) {
+			return imageId;
+		}
+
+		fileName = app.images.idMap[imageId].Filename,
+		tumbnailName = app.config.images.tumbnailPrefix + fileName;
+
+		if (!app.images.nameMap[tumbnailName]) {
+			return imageId;
+		}
+
+		return app.images.nameMap[tumbnailName].Id;
+	}
+
+	app.common = {
+		notification: notification,
+		showLoading: showLoading,
+		hideLoading: hideLoading,
+		navigateToView: navigateToView,
+		resizeImage: resizeImage,
+		updateFilesInfo: updateFilesInfo,
+		getTumbnailIdByImageId: getTumbnailIdByImageId
+	};
 })(window);
