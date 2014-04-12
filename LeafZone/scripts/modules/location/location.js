50fc6353e58b87fd068579954d5c086c282bd3a2
Vladimir Zagorski <vlao85@gmail.com>
2014-04-12 15:35:40 -0500

    add map icons

--- LeafZone/scripts/modules/location/location.js
+++ LeafZone/scripts/modules/location/location.js
@@ -17,6 +17,24 @@
 			var that = this;
 
             app.common.showLoading();
+			
+            var LeafIcon = L.Icon.extend({
+                options: {
+                    shadowUrl: 'styles/images/leaf-shadow.png',
+                    iconSize:     [38, 95],
+                    shadowSize:   [50, 64],
+                    iconAnchor:   [22, 94],
+                    shadowAnchor: [4, 62],
+                    popupAnchor:  [-3, -76]
+                }
+			});
+            
+			var class1 = new LeafIcon({iconUrl: 'styles/images/class1.png'}),
+    			class2 = new LeafIcon({iconUrl: 'styles/images/class2.png'}),
+    			class3 = new LeafIcon({iconUrl: 'styles/images/class3.png'}),
+            	class4 = new LeafIcon({iconUrl: 'styles/images/class4.png'}),
+                class5 = new LeafIcon({iconUrl: 'styles/images/class5.png'}),
+            	class6 = new LeafIcon({iconUrl: 'styles/images/class6.png'});
             
 			var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 				maxZoom: 18,
@@ -31,7 +49,7 @@
             for (var i = 0; i < testArray.length; i++) {
                 var a = testArray[i];
                 var title = a[2];
-                var marker = L.marker(new L.LatLng(a[0], a[1]), { title: title });
+                var marker = L.marker(new L.LatLng(a[0], a[1]), { title: title, icon: class6 });
                 marker.bindPopup(title);
                 markers.addLayer(marker);
             }
