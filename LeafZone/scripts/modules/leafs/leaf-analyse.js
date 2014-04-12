7f746c67af2ce340ab076de5aa86db0169d6460a
Svetozar Toskov <s.toskov@abv.bg>
2014-04-12 08:21:54 -0500

    temporarily remove image resize

--- LeafZone/scripts/modules/leafs/leaf-analyse.js
+++ LeafZone/scripts/modules/leafs/leaf-analyse.js
@@ -94,12 +94,16 @@
             app.newLeafData.discoveredDisease = data.DiseaseName;
             app.newLeafData.ozonePercent = data.OzoneAffected;
             
-            app.common.getResizedImage(data.ImageUrl)
-            .then(function (analyzedlImageData) {
-                app.newLeafData.analyzedlImageData = analyzedlImageData;
+           // app.common.getResizedImage(data.ImageUrl)
+            //.then(function (analyzedlImageData) {
+             //   app.newLeafData.analyzedlImageData = analyzedlImageData;
+              //  app.common.hideLoading();
+               // app.common.navigateToView(app.config.views.leafAnalyse);
+            //});
+            
+              app.newLeafData.analyzedlImageData = app.newLeafData.originalImageData;
                 app.common.hideLoading();
                 app.common.navigateToView(app.config.views.leafAnalyse);
-            });
         },
         
         analyzeImage: function () {
