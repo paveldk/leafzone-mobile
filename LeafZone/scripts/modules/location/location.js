a611769883d5e9e04d80932328b67f23d9b5c6c2
Svetozar Toskov <s.toskov@abv.bg>
2014-04-12 16:43:20 -0500

    extract location method in common module

--- LeafZone/scripts/modules/location/location.js
+++ LeafZone/scripts/modules/location/location.js
@@ -2,11 +2,7 @@
 	var LocationService,
         app = global.app = global.app || {};
 
-	app.newLeafData = app.newLeafData || {};
-
 	LocationService = kendo.Class.extend({
-		viewModel: null,
-
 		init: function () {
 			var that = this;
 
@@ -14,13 +10,24 @@
 		},
 
 		initLocation: function () {
-			var that = this;
-
+			var that = this,
+                LeafIcon,
+                tiles,
+                map,
+                class1,
+    			class2,
+    			class3,
+            	class4,
+                class5,
+            	class6,
+                latlng = L.latLng(-37.82, 175.24),
+                markers = new L.MarkerClusterGroup();
+            
             app.common.showLoading();
 			
-            var LeafIcon = L.Icon.extend({
+            LeafIcon = L.Icon.extend({
                 options: {
-                    shadowUrl: 'styles/images/leaf-shadow.png',
+                    shadowUrl: "styles/images/leaf-shadow.png",
                     iconSize:     [38, 95],
                     shadowSize:   [50, 64],
                     iconAnchor:   [22, 94],
@@ -28,24 +35,21 @@
                     popupAnchor:  [-3, -76]
                 }
 			});
-            
-			var class1 = new LeafIcon({iconUrl: 'styles/images/class1.png'}),
-    			class2 = new LeafIcon({iconUrl: 'styles/images/class2.png'}),
-    			class3 = new LeafIcon({iconUrl: 'styles/images/class3.png'}),
-            	class4 = new LeafIcon({iconUrl: 'styles/images/class4.png'}),
-                class5 = new LeafIcon({iconUrl: 'styles/images/class5.png'}),
-            	class6 = new LeafIcon({iconUrl: 'styles/images/class6.png'});
-            
-			var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-				maxZoom: 18,
-				attribution: ''
-			}),
-			latlng = L.latLng(-37.82, 175.24);
+			
+			class1 = new LeafIcon({iconUrl: 'styles/images/class1.png'});
+			class2 = new LeafIcon({iconUrl: 'styles/images/class2.png'});
+			class3 = new LeafIcon({iconUrl: 'styles/images/class3.png'});
+			class4 = new LeafIcon({iconUrl: 'styles/images/class4.png'});
+			class5 = new LeafIcon({iconUrl: 'styles/images/class5.png'});
+			class6 = new LeafIcon({iconUrl: 'styles/images/class6.png'});
 
-			var map = L.map('map', {center: latlng, zoom: 13, layers: [tiles]});
+            tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+				maxZoom: 18,
+				attribution: ""
+			});		
+
+			map = L.map("map", {center: latlng, zoom: 13, layers: [tiles]});
             
-            var markers = new L.MarkerClusterGroup();
-		
             for (var i = 0; i < testArray.length; i++) {
                 var a = testArray[i];
                 var title = a[2];
